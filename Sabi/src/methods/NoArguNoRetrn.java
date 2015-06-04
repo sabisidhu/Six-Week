89fb971e4d0e9151e383e16756b854c3508e02bd
Sabi <Sabi@abc>
2015-06-04 17:13:26 +0530

    Methods

---
+++ Sabi/src/methods/NoArguNoRetrn.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class NoArguNoRetrn {
+	public static void main(String[] args) {
+		NoArguNoRetrn obj = new NoArguNoRetrn();
+		obj.method();
+	}
+	public void method(){
+		System.out.println("hello");
+	}
+
+}
