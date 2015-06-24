5a4a0b9a8de5cba03140965ef81367514e51075e
Sabi <Sabi@abc>
2015-06-24 09:22:10 +0530

    Update

---
+++ Sabi/src/Conditions/IfElse.java
@@ -0,0 +1,20 @@
+package Conditions;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int a=30;
+		int b=30;
+		int c=50;
+		if(a>b){
+			System.out.println("A is Greater");
+		}
+		if(b>a){
+			System.out.println("B is Greater");
+		}
+		if(a==b){
+			System.out.println("A is equal to B");
+		}
+	}
+	
+
+}
